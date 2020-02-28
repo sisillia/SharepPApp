3e5d122d59049266bdaa83352318dabd7076cd15
Sisillia <sisllia.cahyani@iconpln.co.id>
2020-02-28 14:06:21 +0700

    splash screen activated

--- app/src/main/java/com/example/sharedpapp/SplashActivity.kt
+++ app/src/main/java/com/example/sharedpapp/SplashActivity.kt
@@ -1,12 +1,40 @@
 package com.example.sharedpapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 5000
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_activity)
+
+        mDelayHandler = Handler()
+
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+    override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
     }
 }
